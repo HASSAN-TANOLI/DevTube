4301c2f930455dfe558ed74d448f5e1e9e80f7fd
HASSAN-TANOLI <hassantanoli309@gmail.com>
2022-09-22 01:37:36 +0500

    done with singIn signUp with jwt token

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
+import Jwt from "jsonwebtoken";
 
+//Signup
 export const signup = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
@@ -11,6 +14,30 @@ export const signup = async (req, res, next) => {
 
     await newUser.save();
     res.status(200).send("user has been created");
+  } catch (err) {
+    next(err);
+  }
+};
+
+//SignIn
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) return next(createError(404, "user not found"));
+
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isCorrect) return next(createError(400, "wrong Crendentials"));
+
+    const token = Jwt.sign({ id: user._id }, process.env.JWT);
+
+    const { password, ...others } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
   } catch (err) {
     next(err);
   }
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -3,4 +3,18 @@ import {} from "../controllers/user.js";
 
 const router = express.Router();
 
+//update a user
+
+//delete a user
+
+//get a user
+
+//subscribe a user
+
+//unsubsribe a user
+
+//like a video
+
+//dislike a video
+
 export default router;
